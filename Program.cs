53f519959d6561814933131523e3990f7c4ebd77
L9i7ght4 <126594101+L9i7ght4@users.noreply.github.com>
2023-02-28 20:31:09 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Console_AppN5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+
+            //test
+
+            Console.WriteLine("NA");
+            //com dan sle vide
+
+            //enregistrement
+
+        }
+    }
+}
+
